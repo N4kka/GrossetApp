a6c80724910bde0d85c01ad528512245f2e481e3
Nicola Sarli <sarlinicola7@gmail.com>
2024-04-18 17:51:33 +0200

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace GrossetApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+
+    private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        await Launcher.OpenAsync("https://github.com/n4kka");
+    }
+}
+
